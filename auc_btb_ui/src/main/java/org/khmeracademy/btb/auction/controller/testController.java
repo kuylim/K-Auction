46df34d96d9e1a6e62492705ef3d19346065a788
User <User@User-PC>
2016-08-18 11:42:27 +0700

    hello

---
+++ auc_btb_ui/src/main/java/org/khmeracademy/btb/auction/controller/testController.java
@@ -0,0 +1,13 @@
+
+package org.khmeracademy.btb.auction.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class testController {
+    @RequestMapping("/auc-user")
+    public String auc(){
+        return "admin/auc-admin";
+    }
+}
